862e368443610c986a5b5a4e200f79a93aff7b9d
Josh Holmer <jholmer.in@gmail.com>
2022-10-22 12:33:37 -0400

    Fix XYB conversion

--- src/rgb_xyz.rs
+++ src/rgb_xyz.rs
@@ -1,130 +1,191 @@
-//! Conversion from YUV to XYB requires a few steps. The most direct path is
-//! YUV -> sRGB -> Linear RGB -> XYZ -> LMS -> XYB.
-
 #![allow(clippy::many_single_char_names)]
 
-use nalgebra::{Matrix3, Matrix3x1};
-use num_traits::clamp;
+const K_M02: f32 = 0.078f32;
+const K_M00: f32 = 0.30f32;
+const K_M01: f32 = 1.0f32 - K_M02 - K_M00;
 
-// These are each provided in row-major order
-const XYZ_TO_LINEAR_SRGB_MATRIX: [f32; 9] = [
-    0.7328, 0.4296, -0.1624, -0.7036, 1.6975, 0.0061, 0.0030, 0.0136, 0.9834,
+const K_M12: f32 = 0.078f32;
+const K_M10: f32 = 0.23f32;
+const K_M11: f32 = 1.0f32 - K_M12 - K_M10;
+
+const K_M20: f32 = 0.243_422_69_f32;
+const K_M21: f32 = 0.204_767_45_f32;
+const K_M22: f32 = 1.0f32 - K_M20 - K_M21;
+
+const K_B0: f32 = 0.003_793_073_4_f32;
+const K_B1: f32 = K_B0;
+const K_B2: f32 = K_B0;
+
+const OPSIN_ABSORBANCE_MATRIX: [f32; 9] = [
+    K_M00, K_M01, K_M02, K_M10, K_M11, K_M12, K_M20, K_M21, K_M22,
 ];
-const XYZ_TO_LMS_MATRIX: [f32; 9] = [
-    0.4002, 0.7076, -0.0808, -0.2263, 1.1653, 0.0457, 0.0, 0.0, 0.9182,
+const OPSIN_ABSORBANCE_BIAS: [f32; 3] = [K_B0, K_B1, K_B2];
+const INVERSE_OPSIN_ABSORBANCE_MATRIX: [f32; 9] = [
+    11.031_567_f32,
+    -9.866_944_f32,
+    -0.164_622_99_f32,
+    -3.254_147_3_f32,
+    4.418_770_3_f32,
+    -0.164_622_99_f32,
+    -3.658_851_4_f32,
+    2.712_923_f32,
+    1.945_928_2_f32,
 ];
-const LMS_TO_XYB_MATRIX: [f32; 9] = [1.0, -1.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0];
-
-fn linear_srgb_to_xyz_matrix() -> Matrix3<f32> {
-    xyz_to_linear_srgb_matrix()
-        .try_inverse()
-        .expect("has inverse")
-}
-
-fn xyz_to_lms_matrix() -> Matrix3<f32> {
-    Matrix3::from_row_slice(&XYZ_TO_LMS_MATRIX)
-}
-
-fn lms_to_xyb_matrix() -> Matrix3<f32> {
-    Matrix3::from_row_slice(&LMS_TO_XYB_MATRIX)
-}
-
-fn xyb_to_lms_matrix() -> Matrix3<f32> {
-    lms_to_xyb_matrix().try_inverse().expect("has inverse")
-}
-
-fn lms_to_xyz_matrix() -> Matrix3<f32> {
-    xyz_to_lms_matrix().try_inverse().expect("has inverse")
-}
-
-fn xyz_to_linear_srgb_matrix() -> Matrix3<f32> {
-    Matrix3::from_row_slice(&XYZ_TO_LINEAR_SRGB_MATRIX)
-}
-
-const PLANE_X_MIN: f32 = -0.0979;
-const PLANE_X_MAX: f32 = 0.1799;
-const PLANE_Y_MIN: f32 = 0.0;
-const PLANE_Y_MAX: f32 = 6.1848;
-const PLANE_B_MIN: f32 = 0.0;
-const PLANE_B_MAX: f32 = 6.1808;
+const NEG_OPSIN_ABSORBANCE_BIAS: [f32; 3] = [-K_B0, -K_B1, -K_B2];
 
 /// Converts 32-bit floating point linear RGB to XYB. This function does assume
 /// that the input is Linear RGB. If you pass it gamma-encoded RGB, the results
 /// will be incorrect.
 #[must_use]
 pub fn linear_rgb_to_xyb(input: &[[f32; 3]]) -> Vec<[f32; 3]> {
-    let rgb_to_xyz = linear_srgb_to_xyz_matrix();
-    let xyz_to_lms = xyz_to_lms_matrix();
-    let lms_to_xyb = lms_to_xyb_matrix();
-    let result: Vec<[f32; 3]> = input
+    let mut absorbance_bias = [0.0f32; 3];
+    for (out, bias) in absorbance_bias.iter_mut().zip(OPSIN_ABSORBANCE_BIAS.iter()) {
+        *out = -(bias.cbrt());
+    }
+
+    input
         .iter()
         .map(|pix| {
-            let rgb = Matrix3x1::from_column_slice(pix);
-            let xyz = rgb_to_xyz * rgb;
-            let lms = xyz_to_lms * xyz;
-            let xyb = lms_to_xyb * lms;
-            [
-                clamp(xyb[0], PLANE_X_MIN, PLANE_X_MAX),
-                clamp(xyb[1], PLANE_Y_MIN, PLANE_Y_MAX),
-                clamp(xyb[2], PLANE_B_MIN, PLANE_B_MAX),
-            ]
+            let mut mixed = opsin_absorbance(pix);
+            for (mixed, absorb) in mixed.iter_mut().zip(absorbance_bias.iter()) {
+                if *mixed < 0.0 {
+                    *mixed = 0.0;
+                }
+                *mixed = mixed.cbrt() + (*absorb);
+            }
+            // For wide-gamut inputs, r/g/b and valx (but not y/z) are often negative.
+            mixed_to_xyb(&mixed)
         })
-        .collect();
-
-    result
+        .collect()
 }
 
 /// Converts 32-bit floating point XYB to Linear RGB. This does not perform
 /// gamma encoding on the resulting RGB.
 #[must_use]
 pub fn xyb_to_linear_rgb(input: &[[f32; 3]]) -> Vec<[f32; 3]> {
-    let xyb_to_lms = xyb_to_lms_matrix();
-    let lms_to_xyz = lms_to_xyz_matrix();
-    let xyz_to_rgb = xyz_to_linear_srgb_matrix();
-    let result: Vec<[f32; 3]> = input
-        .iter()
-        .map(|xyb| {
-            let xyb = Matrix3x1::from_column_slice(xyb);
-            let lms = xyb_to_lms * xyb;
-            let xyz = lms_to_xyz * lms;
-            let rgb = xyz_to_rgb * xyz;
-            [rgb[0], rgb[1], rgb[2]]
-        })
-        .collect();
+    let mut biases_cbrt = NEG_OPSIN_ABSORBANCE_BIAS;
+    for bias in &mut biases_cbrt {
+        *bias = bias.cbrt();
+    }
 
-    result
+    input
+        .iter()
+        .map(|pix| {
+            // Color space: XYB -> RGB
+            let mut gamma_rgb = [pix[1] + pix[0], pix[1] - pix[0], pix[2]];
+            for ((rgb, bias_cbrt), neg_bias) in gamma_rgb
+                .iter_mut()
+                .zip(biases_cbrt.iter())
+                .zip(NEG_OPSIN_ABSORBANCE_BIAS.iter())
+            {
+                *rgb -= *bias_cbrt;
+                // Undo gamma compression: linear = gamma^3 for efficiency.
+                let tmp = (*rgb) * (*rgb);
+                *rgb = tmp.mul_add(*rgb, *neg_bias);
+            }
+
+            let mut out = gamma_rgb;
+            out[0] = INVERSE_OPSIN_ABSORBANCE_MATRIX[0] * gamma_rgb[0];
+            out[1] = INVERSE_OPSIN_ABSORBANCE_MATRIX[3] * gamma_rgb[0];
+            out[2] = INVERSE_OPSIN_ABSORBANCE_MATRIX[6] * gamma_rgb[0];
+            out[0] = INVERSE_OPSIN_ABSORBANCE_MATRIX[1].mul_add(gamma_rgb[1], out[0]);
+            out[1] = INVERSE_OPSIN_ABSORBANCE_MATRIX[4].mul_add(gamma_rgb[1], out[1]);
+            out[2] = INVERSE_OPSIN_ABSORBANCE_MATRIX[7].mul_add(gamma_rgb[1], out[2]);
+            out[0] = INVERSE_OPSIN_ABSORBANCE_MATRIX[2].mul_add(gamma_rgb[2], out[0]);
+            out[1] = INVERSE_OPSIN_ABSORBANCE_MATRIX[5].mul_add(gamma_rgb[2], out[1]);
+            out[2] = INVERSE_OPSIN_ABSORBANCE_MATRIX[8].mul_add(gamma_rgb[2], out[2]);
+            out
+        })
+        .collect()
+}
+
+#[inline]
+fn opsin_absorbance(rgb: &[f32; 3]) -> [f32; 3] {
+    let mut out = [0.0f32; 3];
+    out[0] = OPSIN_ABSORBANCE_MATRIX[0].mul_add(
+        rgb[0],
+        OPSIN_ABSORBANCE_MATRIX[1].mul_add(
+            rgb[1],
+            OPSIN_ABSORBANCE_MATRIX[2].mul_add(rgb[2], OPSIN_ABSORBANCE_BIAS[0]),
+        ),
+    );
+    out[1] = OPSIN_ABSORBANCE_MATRIX[3].mul_add(
+        rgb[0],
+        OPSIN_ABSORBANCE_MATRIX[4].mul_add(
+            rgb[1],
+            OPSIN_ABSORBANCE_MATRIX[5].mul_add(rgb[2], OPSIN_ABSORBANCE_BIAS[1]),
+        ),
+    );
+    out[2] = OPSIN_ABSORBANCE_MATRIX[6].mul_add(
+        rgb[0],
+        OPSIN_ABSORBANCE_MATRIX[7].mul_add(
+            rgb[1],
+            OPSIN_ABSORBANCE_MATRIX[8].mul_add(rgb[2], OPSIN_ABSORBANCE_BIAS[2]),
+        ),
+    );
+    out
+}
+
+#[inline]
+fn mixed_to_xyb(mixed: &[f32; 3]) -> [f32; 3] {
+    let mut out = [0.0f32; 3];
+    out[0] = 0.5f32 * (mixed[0] - mixed[1]);
+    out[1] = 0.5f32 * (mixed[0] + mixed[1]);
+    out[2] = mixed[2];
+    out
 }
 
 #[cfg(test)]
 mod tests {
-    use std::{fs::File, path::PathBuf};
+    use std::{
+        fs,
+        path::{Path, PathBuf},
+    };
 
-    use pxm::PFM;
+    use av_data::pixel::{ColorPrimaries, TransferCharacteristic};
 
-    use super::*;
+    use crate::{Rgb, Xyb};
+
+    fn parse_xyb_txt(path: &Path) -> Vec<[f32; 3]> {
+        let input = fs::read_to_string(path).unwrap();
+        input
+            .lines()
+            .map(|l| l.trim())
+            .filter(|l| !l.is_empty())
+            .map(|l| {
+                let (x, l) = l.split_once(' ').unwrap();
+                let (y, b) = l.split_once(' ').unwrap();
+                [x.parse().unwrap(), y.parse().unwrap(), b.parse().unwrap()]
+            })
+            .collect()
+    }
 
     #[test]
-    fn linear_rgb_to_xyb_correct() {
+    fn rgb_to_xyb_correct() {
         let source_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
             .join("test_data")
-            .join("tank_linear_rgb.pfm");
+            .join("tank_srgb.png");
         let expected_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
             .join("test_data")
-            .join("tank_xyb.pfm");
-        let source = PFM::read_from(&mut File::open(source_path).unwrap()).unwrap();
-        let expected = PFM::read_from(&mut File::open(expected_path).unwrap()).unwrap();
+            .join("tank_xyb.txt");
+        let source = image::open(&source_path).unwrap();
         let source_data = source
-            .data
+            .to_rgb32f()
             .chunks_exact(3)
             .map(|chunk| [chunk[0], chunk[1], chunk[2]])
             .collect::<Vec<_>>();
-        let expected_data = expected
-            .data
-            .chunks_exact(3)
-            .map(|chunk| [chunk[0], chunk[1], chunk[2]]);
+        let source = Rgb::new(
+            source_data,
+            1448,
+            1080,
+            TransferCharacteristic::SRGB,
+            ColorPrimaries::BT709,
+        )
+        .unwrap();
+        let expected_data = parse_xyb_txt(&expected_path);
 
-        let result = linear_rgb_to_xyb(&source_data);
-        for (exp, res) in expected_data.into_iter().zip(result.into_iter()) {
+        let result = Xyb::try_from(&source).unwrap();
+        for (exp, res) in expected_data.into_iter().zip(result.data()) {
             assert!(
                 (exp[0] - res[0]).abs() < 0.0005,
                 "Difference in X channel: Expected {:.4}, got {:.4}",
@@ -147,27 +208,25 @@ mod tests {
     }
 
     #[test]
-    fn xyb_to_linear_rgb_correct() {
+    fn xyb_to_rgb_correct() {
         let source_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
             .join("test_data")
-            .join("tank_xyb.pfm");
+            .join("tank_xyb.txt");
         let expected_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
             .join("test_data")
-            .join("tank_linear_rgb.pfm");
-        let source = PFM::read_from(&mut File::open(source_path).unwrap()).unwrap();
-        let expected = PFM::read_from(&mut File::open(expected_path).unwrap()).unwrap();
-        let source_data = source
-            .data
+            .join("tank_srgb.png");
+        let source_data = parse_xyb_txt(&source_path);
+        let source = Xyb::new(source_data, 1448, 1080).unwrap();
+        let expected = image::open(&expected_path).unwrap();
+        let expected_data = expected
+            .to_rgb32f()
             .chunks_exact(3)
             .map(|chunk| [chunk[0], chunk[1], chunk[2]])
             .collect::<Vec<_>>();
-        let expected_data = expected
-            .data
-            .chunks_exact(3)
-            .map(|chunk| [chunk[0], chunk[1], chunk[2]]);
 
-        let result = xyb_to_linear_rgb(&source_data);
-        for (exp, res) in expected_data.into_iter().zip(result.into_iter()) {
+        let result =
+            Rgb::try_from((&source, TransferCharacteristic::SRGB, ColorPrimaries::BT709)).unwrap();
+        for (exp, res) in expected_data.into_iter().zip(result.data()) {
             assert!(
                 (exp[0] - res[0]).abs() < 0.0005,
                 "Difference in R channel: Expected {:.4}, got {:.4}",
