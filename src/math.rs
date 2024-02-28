2eb7809061f59e036efac88892def3163c97bfc8
FreezyLemon <contact@freezylemon.com>
2024-02-28 12:17:41 +0100

    Add attribute to allow non-FMA code once

--- src/math.rs
+++ src/math.rs
@@ -177,6 +177,7 @@ pub fn expf(x: f32) -> f32 {
 
 /// Computes (a * b) + c, leveraging FMA if available
 #[inline]
+#[allow(clippy::suboptimal_flops)]
 pub fn multiply_add(a: f32, b: f32, c: f32) -> f32 {
     if cfg!(target_feature = "fma") {
         a.mul_add(b, c)
